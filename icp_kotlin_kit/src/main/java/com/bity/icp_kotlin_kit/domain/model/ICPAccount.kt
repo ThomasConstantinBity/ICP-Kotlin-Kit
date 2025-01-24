d2a10a0744e0e226d1b84835da3996db848d21af
0xTommy <c.thomas@outlook.it>
2025-01-24 20:08:54 +0100

    update ICPAccount::getTextualRepresentation

--- icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/model/ICPAccount.kt
+++ icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/model/ICPAccount.kt
@@ -1,6 +1,8 @@
 package com.bity.icp_kotlin_kit.domain.model
 
+import com.bity.icp_kotlin_kit.cryptography.CRC32
 import com.bity.icp_kotlin_kit.util.cryptography.ICPAccountCryptography
+import com.google.common.io.BaseEncoding
 
 @OptIn(ExperimentalStdlibApi::class)
 class ICPAccount(
@@ -9,6 +11,21 @@ class ICPAccount(
 ) {
     val accountId = ICPAccountCryptography.generateAccountId(principal, subAccountId)
     val address = accountId.toHexString()
+
+    fun getTextualRepresentation(): String {
+        return if(subAccountId.contentEquals(DEFAULT_SUB_ACCOUNT_ID)) {
+            principal.string
+        } else {
+            val checksum = checksum()
+            val hexSubAccount = subAccountId.toHexString().trimStart { it == '0' }
+            "${principal.string}-$checksum.$hexSubAccount"
+        }
+    }
+
+    private fun checksum(): String {
+        val checksum = CRC32(principal.bytes + subAccountId)
+        return BaseEncoding.base32().encode(checksum)
+    }
 
     companion object {
 
@@ -20,5 +37,6 @@ class ICPAccount(
                 principal = principal,
                 subAccountId = DEFAULT_SUB_ACCOUNT_ID
             )
+        
     }
 }
