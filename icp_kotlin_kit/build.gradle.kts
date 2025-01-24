eb6c1f2d14ebe65e1b2ad175b7a90d0b1d111f99
0xTommy <c.thomas@outlook.it>
2025-01-24 19:33:20 +0100

    update app dependencies

--- icp_kotlin_kit/build.gradle.kts
+++ icp_kotlin_kit/build.gradle.kts
@@ -14,16 +14,13 @@ java {
 dependencies {
     implementation(tegralLibs.niwen.lexer)
     implementation(tegralLibs.niwen.parser)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
-    implementation("com.google.guava:guava:31.0.1-jre")
-    implementation("org.bouncycastle:bcpkix-jdk15on:1.67")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.13.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
-    implementation("com.squareup.retrofit2:converter-jackson:2.7.2")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.10")
+    implementation(libs.bundles.http)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.guava)
+    implementation(libs.bouncycastle)
+    implementation(libs.logging.interceptor)
 
-    testImplementation("io.mockk:mockk:1.13.12")
+    testImplementation(libs.mockk)
     testImplementation(libs.bundles.junit.test)
     testImplementation(libs.bundles.kotlinx.test)
 }
