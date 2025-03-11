d7e8486d73f612394add8bf6d64fb92e4ba7097e
Thomas Constantin <thomas.constantin@bity.com>
2025-03-11 18:59:36 +0100

    add use cases

--- icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/di/DomainModule.kt
+++ icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/di/DomainModule.kt
@@ -1,13 +1,14 @@
 package com.bity.icp_kotlin_kit.di
 
-import com.bity.icp_kotlin_kit.domain.repository.ICPTransactionRepository
 import com.bity.icp_kotlin_kit.domain.use_case.nft.FetchAllNFTCollections
 import com.bity.icp_kotlin_kit.domain.use_case.nft.FetchNFTCollection
 import com.bity.icp_kotlin_kit.domain.use_case.nft.FetchNFTCollectionTokenOwner
 import com.bity.icp_kotlin_kit.domain.use_case.nft.FetchNFTCollectionTokens
 import com.bity.icp_kotlin_kit.domain.use_case.nft.FetchUserNFTTokensHolding
 import com.bity.icp_kotlin_kit.domain.use_case.token.FetchAllTokens
+import com.bity.icp_kotlin_kit.domain.use_case.token.FetchTokenTransactionFee
 import com.bity.icp_kotlin_kit.domain.use_case.token.FetchTokensBalance
+import com.bity.icp_kotlin_kit.domain.use_case.token.SendToken
 import com.bity.icp_kotlin_kit.domain.use_case.transaction.FetchTokenTransactions
 import com.bity.icp_kotlin_kit.domain.use_case.transaction.GetTransactionExplorerUrl
 
@@ -55,6 +56,18 @@ val fetchTokensBalance by lazy {
 
 val fetchAllTokens by lazy {
     FetchAllTokens(
+        tokenRepository = tokenCachedRepository
+    )
+}
+
+val fetchTokenTransactionFee by lazy {
+    FetchTokenTransactionFee(
+        tokenRepository = tokenCachedRepository
+    )
+}
+
+val sendToken by lazy {
+    SendToken(
         tokenRepository = tokenCachedRepository
     )
 }
